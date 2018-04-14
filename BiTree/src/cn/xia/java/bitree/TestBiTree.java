87d97e196010aaf75f68114bcca63aae1a083428
xiarong715 <xiarong2010@163.com>
2018-04-14 21:18:42 +0800

    BiTree

---
+++ BiTree/src/cn/xia/java/bitree/TestBiTree.java
@@ -0,0 +1,37 @@
+package cn.xia.java.bitree;
+
+import java.util.List;
+
+class BitNode {
+	private Object data;
+	private BitNode lChild;
+	private BitNode rChild;
+	public BitNode(Object data) {
+		this.data = data;
+	}
+}
+
+class BiTree {
+	private class BitNode {
+		private Object data;
+		private BitNode lChild;
+		private BitNode rChild;
+		public BitNode(Object data) {
+			this.data = data;
+		}
+	}
+	//***************************
+	private BitNode root; // ¸ù½áµã
+	private static List<BitNode> nodeList;
+	public void createBiTree() {
+		
+	}
+}
+
+public class TestBiTree {
+
+	public static void main(String[] args) {
+
+	}
+
+}
