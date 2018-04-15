b4f007db069528f69f8a401c1b2e4b2bf0f0b93f
xiarong715 <xiarong2010@163.com>
2018-04-15 19:56:27 +0800

    BiTree

--- BiTree/src/cn/xia/java/bitree/TestBiTree.java
+++ BiTree/src/cn/xia/java/bitree/TestBiTree.java
@@ -2,26 +2,37 @@ package cn.xia.java.bitree;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
-class BitNode {
-	private Object data;
-	private BitNode lChild;
-	private BitNode rChild;
-	public BitNode(Object data) {
-		this.data = data;
-	}
-}
+//class BitNode {
+//	private Object data;
+//	private BitNode lChild;
+//	private BitNode rChild;
+//	private boolean visited;
+//	public BitNode(Object data) {
+//		this.data = data;
+//	}
+//	public boolean getVisited() {
+//		return visited;
+//	}
+//	public void setVisited(boolean visited) {
+//		this.visited = visited;
+//	}
+//}
 
 class BiTree {
 	private class BitNode {
 		private Object data;
 		private BitNode lChild;
 		private BitNode rChild;
+		private boolean visited;
 		public BitNode(Object data) {
 			this.data = data;
+			this.visited = false;
 		}
 		public void preOrderNode() {
-			System.out.println(this.data);
+			System.out.print(this.data + " ");
 			if (this.lChild != null) {
 				this.lChild.preOrderNode();
 			}
@@ -33,10 +44,19 @@ class BiTree {
 			if (this.lChild != null) {
 				this.lChild.inOrderNode();
 			}
-			System.out.println(this.data);
+			System.out.print(this.data + " ");
 			if (this.rChild != null) {
 				this.rChild.inOrderNode();
 			}
+		}
+		public void postOrderNode() {
+			if (this.lChild != null) {
+				this.lChild.postOrderNode();
+			}
+			if (this.rChild != null) {
+				this.rChild.postOrderNode();
+			}
+			System.out.print(this.data + " ");
 		}
 	}
 	//***************************
@@ -61,24 +81,97 @@ class BiTree {
 		
 		this.root = nodeList.get(0);
 	}
-	public void preOrder() {  // 前序编历
+	public void preOrderD() {  // 前序编历
 		if (this.root != null) {
 			this.root.preOrderNode();
 		}
 	}
-	public void inOrder() {
+	public void inOrderD() {
 		if (this.root != null) {
 			this.root.inOrderNode();
+		}
+	}
+	public void postOrderD() {
+		if (this.root != null) {
+			this.root.postOrderNode();
+		}
+	}
+	public void preOrder() {
+		if (this.root != null) {
+			Stack<BitNode> stack = new Stack<BitNode>(); 
+			BitNode b = this.root;
+			while(b != null || !stack.isEmpty()) {
+				if (b != null) {
+					System.out.print(b.data+ " ");
+					stack.push(b);
+					b = b.lChild;
+				} else {
+					b = stack.pop();
+					b = b.rChild;
+				}
+			}
+		}
+	}
+	public void inOrder() { // 中序编历，非递归 
+		if (this.root != null) {
+			Stack<BitNode> stack = new Stack<BitNode>();
+			BitNode b = this.root;
+			while (b != null || !stack.isEmpty()) {
+				if (b != null) {
+					stack.push(b);
+					b = b.lChild;
+				} else {
+					b = stack.pop();
+					System.out.print(b.data + " ");
+					b = b.rChild;
+				}
+			}
+		}
+	}
+	public void postOrder() {
+		if (this.root != null) {
+			Stack<BitNode> stack = new Stack<BitNode>();
+			BitNode b = this.root;
+			while (b != null || !stack.isEmpty()) {
+				if (b != null) {
+					stack.push(b);
+					b = b.lChild;
+				} else {
+					b = stack.peek(); // 偷看一眼
+					if (b.rChild != null && b.visited == false) { //保证每个节点访问一次
+						b.visited = true;
+						b = b.rChild;
+					} else {  // 没有右孩子，则出栈并访问 
+						b = stack.pop();
+						System.out.print(b.data + " ");
+						b = null;  // 没有右孩子，且访问过了，
+					}
+				}
+			}
+		}
+	}
+	public void levelOrder() {
+		Queue<BitNode> queue = new LinkedList<BitNode>();
+		if (this.root != null) {
+			queue.add(this.root);
+			while (!queue.isEmpty()) {
+				BitNode bitNode = queue.poll();
+				System.out.print(bitNode.data + " ");
+				if (bitNode.lChild != null) {
+					queue.add(bitNode.lChild);
+				}
+				if (bitNode.rChild != null) {
+					queue.add(bitNode.rChild);
+				}
+			}
 		}
 	}
 }
 
 public class TestBiTree {
-
 	public static void main(String[] args) {
 		BiTree bT = new BiTree();
 		bT.createBiTree();
-		bT.inOrder();
+		bT.postOrder();
 	}
-
 }
