b760d29295820e6b9ccdd91b924872f39d279be8
xiarong715 <xiarong2010@163.com>
2018-04-16 21:13:47 +0800

    link reverse reverse

--- linkReverse/src/cn/xia/java/linkReverse/LinkReverse.java
+++ linkReverse/src/cn/xia/java/linkReverse/LinkReverse.java
@@ -1,13 +1,62 @@
 package cn.xia.java.linkReverse;
 
-import java.util.LinkedList;
-import java.util.List;
+class Node {
+	private Integer data;
+	private Node next;
+	public Node(Integer data) {
+		this.data = data;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node getNext() {
+		return this.next;
+	}
+	public static void print(Node head) {
+		if (head == null) {
+			return ;
+		}
+		Node p = head.next;
+		while (p != null) {
+			System.out.print(p.data + " ");
+			p = p.next;
+		}
+		System.out.println();
+	}
+	public static Node reverseList(Node head) {
+		if (head.next == null || head.next.next == null) {
+			return head;
+		}
+		Node pre = null;
+		Node p = head.next;
+		Node latter = head.next.next;
+		while (p != null) {
+			p.next = pre;
+			pre = p;
+			p = latter;
+			if (p != null) {
+				latter = latter.next;
+			}
+		}
+		head.next = pre;
+		
+		return head;
+	}
+}
 
 public class LinkReverse {
-
 	public static void main(String[] args) {
-		List<Integer> list = new LinkedList<Integer>(); 
-		
+		Node head = new Node(-1);
+		Node p = head;
+		for (int x = 0; x <= 10; x ++) {
+			Node q = new Node(x);
+			p.setNext(q);
+			p = q;
+		}
+		Node.print(head);
+		System.out.println("**********************************");
+		Node.reverseList(head);
+		Node.print(head);
 	}
 
 }
